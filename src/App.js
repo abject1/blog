e6932e767b5adca5ff8b494f43f82e4501bfec30
Abject <lucaswurtz0@gmail.com>
2022-05-05 18:00:43 +0200

    fin de mise en place des fichier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// Import
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Homes";
+import About from "./pages/About";
+
+// Boucle App
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Error />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Hello Error</h1>
+    </div>
+  );
+};
+
+export default Error;
